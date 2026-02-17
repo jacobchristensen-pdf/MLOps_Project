21b5b613a06ec870e389dcdd5b2e2b2dcdcb836e
jacobchristensen-pdf <jacob@digiline.dk>
2026-02-17 13:44:56 +0100

    Tilføjet kommentar

--- tests/test_data.py
+++ tests/test_data.py
@@ -12,6 +12,7 @@ from pydeequ.verification import VerificationSuite
 
 
 def test_dataset():
+    # Spark session for deequ
     spark = (
         SparkSession.builder
         .master("local[*]")
@@ -20,11 +21,13 @@ def test_dataset():
         .getOrCreate()
     )
 
+    # Dummy dataset - has to be changed to work with data infrastructure
     data = [
         ("img1.jpg", "cat", 224, 224),
         ("img2.jpg", "dog", 224, 224),
         ("img3.jpg", "cat", 224, 224),
     ]
+    # Spark dataframe for data validation
     df = spark.createDataFrame(data, ["image_path", "label", "width", "height"])
 
     result = (
@@ -32,12 +35,12 @@ def test_dataset():
         .onData(df)
         .addCheck(
             Check(spark, CheckLevel.Error, "dataset validation")
-            .hasSize(lambda x: x > 0)
-            .isComplete("image_path")
-            .isUnique("image_path")
-            .isContainedIn("label", ["cat", "dog"])
-            .isNonNegative("width")
-            .isNonNegative("height")
+            .hasSize(lambda x: x > 0)               # Data must have at least 1 row/image
+            .isComplete("image_path")               # Each image path has to be available
+            .isUnique("image_path")                 # No duplicates
+            .isContainedIn("label", ["cat", "dog"]) # Must have either "cat" or "dog" labels
+            .isNonNegative("width")                 # Image with must be positive
+            .isNonNegative("height")                # Image height must be positive
         )
         .run()
     )
